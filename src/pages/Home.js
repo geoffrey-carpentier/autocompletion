36f3d440fb13298d09795ffcfe9647f8ad695649
Geoffrey CARPENTIER <geoffrey.carpentier@laplateforme.io>
2026-01-21 11:45:15 +0100

    Ajout de la dépendance react-router-dom et création de la structure initiale des composants - Ajout de react-router-dom aux fichiers package.json et package-lock.json - Suppression de fichiers inutilisés App.test.js et logo.svg - Création de nouveaux composants :  Header, SearchBar, Home, RecipeDetail et SearchResults + leurs fichiers CSS respectifs

---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+// Composant de la Page d'Accueil (Home)
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>Page d'Accueil</h1>
+      <p> Barre de recherche à venir.</p>
+    </div>
+  );
+}
---
+++ src/pages/SearchResults.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './SearchResults.css';
+
+// Composant de la Page de Résultats (SearchResults)
+export default function SearchResults() {
+  return (
+    <div className="search-results">
+      <h1>Résultats de recherche</h1>
+      <p>La liste des recettes s'affichera ici.</p>
+    </div>
+  );
+}
